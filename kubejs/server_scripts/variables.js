ed435430ca917ab78efb3b1fb30a1d20de7975b4
DevDyna <andrearisorti2003@gmail.com>
2024-07-16 15:37:23 +0200

    all stuff

---
+++ kubejs/server_scripts/variables.js
@@ -0,0 +1,207 @@
+//priority 99
+
+let scar = Item.of(
+  "tacz:modern_kinetic_gun",
+  '{AttachmentEXTENDED_MAG:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:extended_mag_3"}},AttachmentGRIP:{Count:0b,id:"minecraft:air",tag:{glide:0b,struck:0b}},AttachmentMUZZLE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:muzzle_brake_cthulhu"}},AttachmentSCOPE:{Count:0b,id:"minecraft:air",tag:{glide:0b,struck:0b}},AttachmentSTOCK:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:stock_tactical_ar"}},GunCurrentAmmoCount:30,GunFireMode:"SEMI",GunId:"tacz:scar_h",HasBulletInBarrel:1b}'
+);
+let gaus = Item.of(
+  "tacz:modern_kinetic_gun",
+  '{GunCurrentAmmoCount:20,GunFireMode:"SEMI",GunId:"lradd:gauss_rifle",HasBulletInBarrel:1b}'
+);
+
+let m4a1 = Item.of(
+  "tacz:modern_kinetic_gun",
+  '{AttachmentEXTENDED_MAG:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:extended_mag_1"}},AttachmentGRIP:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:grip_vertical_talon"}},AttachmentMUZZLE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:muzzle_brake_trex"}},AttachmentSCOPE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:sight_t2"}},AttachmentSTOCK:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:stock_carbon_bone_c5"}},GunCurrentAmmoCount:40,GunFireMode:"AUTO",GunId:"tacz:m4a1",HasBulletInBarrel:1b}'
+);
+let akm = Item.of(
+  "tacz:modern_kinetic_gun",
+  '{AttachmentEXTENDED_MAG:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:extended_mag_1"}},AttachmentMUZZLE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:muzzle_brake_trex"}},AttachmentSCOPE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:sight_coyote"}},AttachmentSTOCK:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:stock_carbon_bone_c5"}},GunCurrentAmmoCount:34,GunFireMode:"AUTO",GunId:"tacz:ak47",HasBulletInBarrel:1b}'
+);
+let nitro_505 = Item.of(
+  "tacz:modern_kinetic_gun",
+  '{AttachmentSCOPE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:scope_standard_8x"}},GunCurrentAmmoCount:1,GunFireMode:"SEMI",GunId:"lradd:nitro_505",HasBulletInBarrel:1b}'
+);
+let m200 = Item.of(
+  "tacz:modern_kinetic_gun",
+  '{AttachmentMUZZLE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:muzzle_brake_trex"}},AttachmentSCOPE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:scope_standard_8x"}},GunCurrentAmmoCount:10,GunFireMode:"SEMI",GunId:"lradd:m200",HasBulletInBarrel:1b}'
+);
+let m95 = Item.of(
+  "tacz:modern_kinetic_gun",
+  '{AttachmentEXTENDED_MAG:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:sniper_extended_mag_1"}},AttachmentSCOPE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:scope_lpvo_1_6"}},GunCurrentAmmoCount:6,GunFireMode:"SEMI",GunId:"tacz:m95",HasBulletInBarrel:1b}'
+);
+let vector45 = Item.of(
+  "tacz:modern_kinetic_gun",
+  '{AttachmentEXTENDED_MAG:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:light_extended_mag_1"}},AttachmentGRIP:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:grip_vertical_talon"}},AttachmentMUZZLE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:muzzle_brake_trex"}},AttachmentSCOPE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:sight_coyote"}},AttachmentSTOCK:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:stock_carbon_bone_c5"}},GunCurrentAmmoCount:24,GunFireMode:"AUTO",GunId:"tacz:vector45",HasBulletInBarrel:1b}'
+);
+let nailgun = Item.of(
+  "tacz:modern_kinetic_gun",
+  '{AttachmentEXTENDED_MAG:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:extended_mag_3"}},AttachmentMUZZLE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:muzzle_brake_trex"}},AttachmentSCOPE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:sight_coyote"}},AttachmentSTOCK:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:stock_carbon_bone_c5"}},GunCurrentAmmoCount:50,GunFireMode:"AUTO",GunId:"lradd:nailgun",HasBulletInBarrel:1b}'
+);
+let deagle_golden = Item.of(
+  "tacz:modern_kinetic_gun",
+  '{AttachmentEXTENDED_MAG:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:light_extended_mag_1"}},AttachmentMUZZLE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:deagle_golden_long_barrel"}},AttachmentSCOPE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:sight_t1"}},GunCurrentAmmoCount:12,GunFireMode:"SEMI",GunId:"tacz:deagle_golden",HasBulletInBarrel:1b}'
+);
+let glock_17 = Item.of(
+  "tacz:modern_kinetic_gun",
+  '{AttachmentEXTENDED_MAG:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:light_extended_mag_1"}},AttachmentMUZZLE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:muzzle_silence_mirage"}},AttachmentSCOPE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:sight_sro_dot"}},GunCurrentAmmoCount:20,GunFireMode:"SEMI",GunId:"tacz:glock_17",HasBulletInBarrel:1b}'
+);
+let aa12 = Item.of(
+  "tacz:modern_kinetic_gun",
+  '{AttachmentEXTENDED_MAG:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:extended_mag_1"}},AttachmentGRIP:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:grip_vertical_talon"}},AttachmentSCOPE:{Count:1b,id:"tacz:attachment",tag:{AttachmentId:"tacz:sight_coyote"}},GunCurrentAmmoCount:16,GunFireMode:"SEMI",GunId:"tacz:aa12",HasBulletInBarrel:1b}'
+);
+let db_long = Item.of(
+  "tacz:modern_kinetic_gun",
+  '{GunCurrentAmmoCount:2,GunFireMode:"SEMI",GunId:"tacz:db_long",HasBulletInBarrel:1b}'
+);
+let rpg = Item.of(
+  "tacz:modern_kinetic_gun",
+  '{GunCurrentAmmoCount:1,GunFireMode:"SEMI",GunId:"tacz:rpg7",HasBulletInBarrel:1b}'
+);
+let nuclear_202 = Item.of(
+  "tacz:modern_kinetic_gun",
+  '{GunCurrentAmmoCount:4,GunFireMode:"SEMI",GunId:"lradd:m202",HasBulletInBarrel:1b}'
+);
+let machinegun = Item.of(
+  "tacz:modern_kinetic_gun",
+  '{GunCurrentAmmoCount:75,GunFireMode:"AUTO",GunId:"tacz:m249",HasBulletInBarrel:1b}'
+);
+let hp_point = "kubejs:hp_point";
+let jetpack = "jetboots:rocket_boots";
+let enderstaff = "grapplemod:launcheritem";
+let grapple_dual = Item.of(
+  "grapplemod:grapplinghook",
+  "{Damage:0,custom:{angle:25,doublehook:1b,hookgravity:50,maxlen:60,motor:1b,motormaxspeed:10,motorwhencrouching:1b,playermovementmult:2,reelin:0b,rocket:1b,rocket_vertical_angle:30,smartdoublemotor:1b,sneakingangle:0,sneakingverticalthrowangle:25,sticky:1b,throwspeed:20,verticalthrowangle:30}}"
+);
+let grapple_mono = Item.of(
+  "grapplemod:grapplinghook",
+  "{Damage:0,custom:{maxlen:60,motor:1b,playermovementmult:2,throwspeed:3.5d}}"
+);
+let legs = (level, name, color, affix, bold) =>
+  Item.of(
+    "jetboots:guardian_pants",
+    '{display:{Name:\'{"text":"' +
+      affix +
+      " " +
+      name +
+      " Guardian Pants " +
+      affix +
+      '","color":"#' +
+      color +
+      '","bold":' +
+      bold +
+      ',"italic":false}\'},bml-item-level:10,bml-item-xp:4,jetboots-enchantable-container:{Items:[{Count:1b,Slot:0,id:"minecraft:enchanted_book",tag:{StoredEnchantments:[{id:"minecraft:protection",lvl:' +
+      level +
+      "s}]}}],Size:3}}"
+  ).enchant("minecraft:protection", level);
+let chest = (level, name, color, affix, bold) =>
+  Item.of(
+    "jetboots:guardian_jacket",
+    '{display:{Name:\'{"text":"' +
+      affix +
+      " " +
+      name +
+      " Guardian Jacket " +
+      affix +
+      '","color":"#' +
+      color +
+      '","bold":' +
+      bold +
+      ',"italic":false}\'},bml-item-level:10,bml-item-xp:4,jetboots-enchantable-container:{Items:[{Count:1b,Slot:0,id:"minecraft:enchanted_book",tag:{StoredEnchantments:[{id:"minecraft:protection",lvl:' +
+      level +
+      "s}]}}],Size:3}}"
+  ).enchant("minecraft:protection", level);
+
+let armor = (level, name, color, affix, bold) => [
+  chest(level, name, color, affix, bold),
+  legs(level, name, color, affix, bold),
+];
+
+let _12_gauge = Item.of(
+  "tacz:ammo_box",
+  '{AmmoCount:108,AmmoId:"tacz:12g",Level:0}'
+);
+
+let _556x45 = Item.of(
+  "tacz:ammo_box",
+  '{AmmoCount:180,AmmoId:"tacz:556x45",Level:0}'
+);
+
+let _rpg_rocket = Item.of(
+  "tacz:ammo_box",
+  '{AmmoCount:18,AmmoId:"tacz:rpg_rocket",Level:0}'
+);
+
+let _9mm = Item.of(
+  "tacz:ammo_box",
+  '{AmmoCount:180,AmmoId:"tacz:9mm",Level:0}'
+);
+
+let _nail = Item.of(
+  "tacz:ammo_box",
+  '{AmmoCount:180,AmmoId:"lradd:nail",Level:0}'
+);
+
+let _arrow_crossbow = Item.of(
+  "tacz:ammo_box",
+  '{AmmoCount:180,AmmoId:"lradd:arrow",Level:0}'
+);
+
+let _357_magnum = Item.of(
+  "tacz:ammo_box",
+  '{AmmoCount:180,AmmoId:"tacz:magnum_r",Level:0}'
+);
+
+let _338_lapua = Item.of(
+  "tacz:ammo_box",
+  '{AmmoCount:90,AmmoId:"tacz:338",Level:0}'
+);
+
+let _45_acp = Item.of(
+  "tacz:ammo_box",
+  '{AmmoCount:180,AmmoId:"tacz:45acp",Level:0}'
+);
+
+let _50_bmg = Item.of(
+  "tacz:ammo_box",
+  '{AmmoCount:90,AmmoId:"tacz:50bmg",Level:0}'
+);
+
+let _762x39 = Item.of(
+  "tacz:ammo_box",
+  '{AmmoCount:180,AmmoId:"tacz:762x39",Level:0}'
+);
+
+let _308_winchester = Item.of(
+  "tacz:ammo_box",
+  '{AmmoCount:144,AmmoId:"tacz:308",Level:0}'
+);
+
+let _2mm_ec = Item.of(
+  "tacz:ammo_box",
+  '{AmmoCount:180,AmmoId:"lradd:2mm_ec",Level:0}'
+);
+
+let kit_machinegun = [_556x45, machinegun];
+let kit_nuclear_202 = [_rpg_rocket, nuclear_202];
+let kit_rpg = [_rpg_rocket, rpg];
+let kit_db_long = [_12_gauge, db_long];
+let kit_aa12 = [_12_gauge, aa12];
+let kit_glock_17 = [_9mm, glock_17];
+let kit_deagle_golden = [_357_magnum, deagle_golden];
+let kit_nailgun = [_nail, nailgun];
+let kit_vector45 = [_45_acp, vector45];
+let kit_m4a1 = [_556x45, m4a1];
+let kit_akm = [_762x39, akm];
+let kit_nitro_505 = [_arrow_crossbow, nitro_505];
+let kit_m200 = [_338_lapua, m200];
+let kit_m95 = [_50_bmg, m95];
+let kit_scar = [_308_winchester, scar];
+let kit_gaus = [_2mm_ec, gaus];
+
+let xp = () => {
+  let result = [];
+  for (let i = 1; i < 65; i ++) {
+    result.push(i + "x " + hp_point);
+  }
+  return result;
+};
